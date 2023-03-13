0ee69ef841fe59ac6034cf083625a074ff7c3d19
Sajay Prakash <sajayprakash7@protonmail.com>
2023-03-13 17:57:50 +0530

    initial changes: add toggle theme button

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog - Sajay Prakash';
+export const SITE_DESCRIPTION = 'A site where I post my blogs about tech and sometimes non-tech related stuff';
+export const HOMEPAGE_URL = 'https://blog.sajayprakash.com';
